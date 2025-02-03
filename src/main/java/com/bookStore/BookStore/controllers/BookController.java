dc4510a3ec336d2ac57e19862a242945ba1af947
caioo <matossousacaio.cms@gmail.com>
2025-02-03 18:45:03 -0300

    Add a 2 methods: createBook for page book register and getAllBooks for page availableBook

--- src/main/java/com/bookStore/BookStore/controllers/BookController.java
+++ src/main/java/com/bookStore/BookStore/controllers/BookController.java
@@ -1,11 +1,24 @@
 package com.bookStore.BookStore.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.bookStore.BookStore.entities.Book;
+import com.bookStore.BookStore.services.BookService;
+
 
 @Controller
 public class BookController {
     
+    @Autowired
+    private BookService bookService;
+
     @GetMapping("/")
     public String home() {
         return "home";
@@ -15,10 +28,18 @@ public class BookController {
     public String bookRegister() {
         return "bookRegister";
     }
+
+    @PostMapping("/bookRegister")
+    public String createBook(@ModelAttribute Book book) {
+        bookService.save(book);
+        
+        return "redirect:/available_book";
+    }
     
     @GetMapping("/available_book")
-    public String getAllBooks() {
-        return "availableBook";
+    public ModelAndView getAllBooks() {
+        List<Book> list = bookService.allBooks();
+        return new ModelAndView("availableBook", "book", list);
     }
     
 }
--- src/main/java/com/bookStore/BookStore/services/BookService.java
+++ src/main/java/com/bookStore/BookStore/services/BookService.java
@@ -1,8 +1,30 @@
 package com.bookStore.BookStore.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.bookStore.BookStore.entities.Book;
+import com.bookStore.BookStore.repositories.BookRepository;
 
 @Service
 public class BookService {
+ 
+    @SuppressWarnings("FieldMayBeFinal")
+    private BookRepository bookRepository;
     
+    //dependence injection
+    public BookService(BookRepository bookRepository){
+        this.bookRepository = bookRepository;
+    }
+
+    // method for save a new book
+    public void save(Book book){
+        bookRepository.save(book);
+    }
+
+    // method for list all books
+    public List<Book> allBooks(){
+        return bookRepository.findAll();
+    }
 }
